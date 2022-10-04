3d6f56db6f0fd86ab0c499d5b2dea6584ce61c08
Duy K. Bui <duy@buifamily.info>
2022-10-04 14:31:03 -0400

    fix: Preview href no longer hard-coded

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -27,7 +27,7 @@ export default function Preview(
           (
             <div>
               <a
-                href="https://github.com/khuongduybui/fresh-turnstile"
+                href={href}
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 target="_blank"
               >
