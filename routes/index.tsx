48b37782b2c5860220a82e3379b2e195a8c49364
Duy K. Bui <duy@buifamily.info>
2022-10-04 14:28:17 -0400

    feat: add header

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ import Preview from "../components/Preview.tsx";
 export default function Home() {
   const pageCode = `<Page [title="Page Title"]>...</Page>`;
   const pagePreview = (
-    <Preview header="<Page/>" sourceCode={pageCode}>
+    <Preview header="<Page/>" sourceCode={pageCode} href="https://github.com/khuongduybui/fresh-flowbite">
       There is no preview for a page. It simply puts title into head and classes on body.
     </Preview>
   );
@@ -21,7 +21,7 @@ export default function Home() {
 <Button type="button" variant="green">Primary</Button>
 <Button type="button" variant="purple">Primary</Button>`;
   const buttonPreview = (
-    <Preview header="<Button/>" sourceCode={buttonCode}>
+    <Preview header="<Button/>" sourceCode={buttonCode} href="https://github.com/khuongduybui/fresh-flowbite">
       <Button type="button">Default</Button>
       <Button type="button" variant="primary">Primary</Button>
       <Button type="button" variant="dark">Primary</Button>
@@ -51,6 +51,19 @@ export default function Home() {
   return (
     <Page title="Documentation for Fresh Flowbite">
       <div class="p-4 mx-auto max-w-screen-lg">
+        <div>
+          <p>This site is a demo / docs page for my various Deno Fresh plugins. Source codes for the site as well as those plugins are all MIT-licensed.</p>
+          <p class="pt-4">
+            <a
+              href="https://github.com/khuongduybui/fresh-flowbite-site"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              target="_blank"
+            >
+              View on GitHub
+            </a>
+          </p>
+        </div>
+        <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" />
         {pagePreview}
         {buttonPreview}
         {cfTurnstilePreview}
