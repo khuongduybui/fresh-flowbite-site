8d371ee20c9650d08c6b70abb9b33620ec778541
Duy K. Bui <duy@buifamily.info>
2022-10-16 14:44:33 -0400

    feat: Use combined log format

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,8 @@
-import { getLogger, ResolutionField } from "$logging/index.ts";
+import { getLogger, LoggingFormat, ResolutionField } from "$logging/index.ts";
 
 export const handler = [
   getLogger({
+    format: LoggingFormat.APACHE_COMBINED,
     resolvers: {
       [ResolutionField.bytes]: async (_req, _ctx, res) => `${(await res.clone().arrayBuffer()).byteLength}`,
     },
