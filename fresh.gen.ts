361b898db34f79b597a46c12d735eb5ba1ee8dae
Duy K. Bui <duy@buifamily.info>
2023-06-17 12:30:53 -0400

    chore: bump fresh to 1.2

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/_middleware.ts";
 import * as $2 from "./routes/cf-turnstile-validation.tsx";
@@ -22,7 +21,6 @@ const manifest = {
     "./islands/CfTurnstileForm.tsx": $$1,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
